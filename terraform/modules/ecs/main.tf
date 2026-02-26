52307f565b98a3515d3e455ed3885bf654a28fdd
heyrohhh <rm8619700@gmail.com>
2026-02-26 18:09:52 +0530

    added telegram bot

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy" "execution_role_secrets" {
         Action   = ["secretsmanager:GetSecretValue"]
        Resource = [
           "arn:aws:secretsmanager:us-east-1:985017008178:secret:GF_SECURITY_ADMIN_USER-xMu4ly",
-          "arn:aws:secretsmanager:us-east-1:985017008178:secret:GF_SECURITY_ADMIN_PASSWORD-Rwxzvg"
+          "arn:aws:secretsmanager:us-east-1:985017008178:secret:GF_SECURITY_ADMIN_PASSWORD-Rwxzvg",
            "arn:aws:secretsmanager:us-east-1:985017008178:secret:TELEGRAM_BOT_TOKEN-BTvHit"
         ]
       }
