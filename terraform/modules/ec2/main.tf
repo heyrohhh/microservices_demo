934cf38ca602b4466c81aedbcf46785777d79171
heyrohhh <rm8619700@gmail.com>
2026-02-28 13:08:27 +0530

    alowed bastion for prometheus

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -16,8 +16,8 @@ resource "aws_security_group" "ssm_bastion_sg" {
   vpc_id = var.vpc_id                               
 
   egress {
-    from_port= 443                              
-    to_port= 443
+    from_port= 0                            
+    to_port= 0
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
